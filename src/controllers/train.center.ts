77d999807921efaa4e52306a1d6c5df1f6a6a547
chsjen492 <chsjen492@gmail.com>
2024-07-16 08:44:09 +0000

    feat: add search traincenter api
    delete duplicate router
    
    modify file path
    
    fix error
    
    feat: add search traincenter api
    
    
    Merge-request: REBRIDGE-MR-39
    Merged-by: 미나 이 <chsjen492@gmail.com>

--- src/controllers/train.center.ts
+++ src/controllers/train.center.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { TrainCenterModel } from '@models/models'
+import { InternalServerError } from '@/types/errors'
 export async function centerHealthCheck(req: Request, res: Response) {
     res.status(200).send('OK')
 }
@@ -10,4 +11,17 @@ export async function centerGetDetails(req: Request, res: Response) {
         res.status(404).json({ message: 'Train center not found' })
     }
     res.status(200).json(trainCenter)
+}
+
+export async function searchTrainCenter(req: Request, res: Response) {
+    const query = req.query.value
+    if (!query) {
+        throw new Error('Search value is required')
+    }
+
+    const result = await TrainCenterModel.find({ inoNm: { $regex: new RegExp(`${query}`, 'i') } })
+
+    if (!result) {
+        throw new InternalServerError()
+    }
 }
